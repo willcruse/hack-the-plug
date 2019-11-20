b1af0d279e914366fd8db540188a9557e634fd1d
Will Cruse <willcruse123@gmail.com>
2019-11-20 09:13:03 -0500

    Change travel message

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const IndexPage = () => (
     <div id="FAQs">
       <h1>FAQs</h1>
       <QA question="When is Hack The Plug?" ans="Hack The Plug will be held from the 28th to the 29th of March 2020" />
-      <QA question="Will you provide travel reimbusment?" ans="We aim to reimburse travel up to £50 for those inside the UK and up to £300 for those outside the UK" />
+      <QA question="Will you provide travel reimbusment?" ans="We aim to provide travel reimbursement" />
       <QA question="How much does Hack the Plug cost to attend?" ans="Hack the Plug is completely free to attend!" />
     </div>
     <div id="Sponsor">
