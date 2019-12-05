2916743c1ce5a9f2007a5cfb378008252c3f5e23
Oliver <oliver.redeyoff@gmail.com>
2019-12-05 14:48:27 +0000

    Solved wrong component reference error

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import StyledBackgroundSection from "../components/test_component"
+import StyledBackgroundSection from "../components/bg"
 import '../components/about'
 import Team from "../components/Team"
 import FAQs from "../components/FAQs"
@@ -11,10 +11,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div id="About">
-        <h1>About Hack the Plug</h1>
-        <p>
-          Hack The Plug is a student run, 24 Hour hackathon held at the University of Bath. Come join us to learn something new, build something cool or start a company!
-        </p>
+      <h1>About Hack the Plug</h1>
+      <p>Hack The Plug is a student run, 24 Hour hackathon held at the University of Bath. Come join us to learn something new, build something cool or start a company!</p>
     </div>
     <Team />
     <FAQs />
