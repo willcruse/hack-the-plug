0622abc4f1c58b930fe1d169da1acab891b148ce
Oliver <oliver.redeyoff@gmail.com>
2019-12-05 14:46:06 +0000

    Updated header, background image component renamed and formated code

--- src/components/header.js
+++ src/components/header.js
@@ -1,74 +1,105 @@
-import { Link } from "gatsby"
+import {
+  Link
+} from "gatsby"
 import Image from "../components/image"
 import PropTypes from "prop-types"
 import React from "react"
-import { Container, Row, Col } from "react-bootstrap"
-const Header = () => (
-  <div>
-    <header
-      style={{
-        background: `#008dd5`,
-        // marginBottom: `1.45rem`
-      }}
-    >
-      <Container fluid>
-        <Row>
-          <Col style={{
-            maxHeight: `300px`,
-            maxWidth: `300px`
-          }}>
-            <Image alt="logo" />
-          </Col>
-          <Col></Col>
-          <Col style={{ alignSelf: `flex-end` }}>
-            <h2>
-              <Link
-                to="/"
-                style={{
-                  color: `white`,
-                  float: `bottom`,
-                }}
-              >
-                Home
-      </Link>
-            </h2>
-          </Col>
-          {/* <Col style={{ alignSelf: `flex-end` }}>
-            <h2>
-              <Link
-                to="/committee"
-                style={{
-                  color: `white`,
-                  float: `bottom`,
-                }}>Committee</Link>
-                </h2>
-          </Col> */}
-            {/* The Link to the application page */}
-            {/* <Col style={{alignSelf: `flex-end`}}>
-      <h2>
+import {
+  Container,
+  Row,
+  Col
+} from "react-bootstrap"
+const Header = () => ( <div>
+  <header style = {
+    {
+      background: `#008dd5`,
+      // marginBottom: `1.45rem`
+    }
+  }>
+  <Container fluid>
+    <Row style = {{maxWidth: '50%', marginLeft: '10%'}}>
+      {/*<Col style = {
+        {
+          maxHeight: `300px`,
+          maxWidth: `300px`
+        }
+      } >
+      <Image alt = "logo" />
+
+
+      </Col>
+        <Col> < /Col>*/}
+
+
+        {/* The Link to the About section */ }
+        <Col style = {{alignSelf: `flex-end`, textAlign: 'center'}}>
+        <h2>
+        <Link
+          to = "/"
+          style = {{
+            color: `white`,
+            float: `bottom`,
+            fontSize: 14
+          }}>About</Link>
+        </h2>
+        </Col>
+
+
+        {/* The Link to the committee section */ }
+        <Col style={{alignSelf: `flex-end`, textAlign: 'center'}}>
+        <h2>
+        <Link
+          to="/committee"
+          style={{
+            color: `white`,
+            float: `bottom`,
+            fontSize: 14
+          }}>The team</Link>
+        </h2>
+        </Col>
+
+
+        {/* The Link to the application section */ }
+        <Col style={{alignSelf: `flex-end`, textAlign: 'center'}}>
+        <h2>
         <Link
           to="/apply"
           style={{
             color: `white`,
-          }}
-        >
-          Apply
-      </Link>
-      </h2>
-        </Col>*/}
-        </Row>
-      </Container>
-    </header>
+            float: `bottom`,
+            fontSize: 14
+          }}>FAQs</Link>
+        </h2>
+        </Col>
+
+
+        {/* The Link to the sponsors section */ }
+        <Col style={{alignSelf: `flex-end`, textAlign: 'center'}}>
+        <h2>
+        <Link
+          to="/apply"
+          style={{
+            color: `white`,
+            float: `bottom`,
+            fontSize: 14
+          }}>Sponsors</Link>
+        </h2>
+        </Col>
+
+
+    </Row>
+  </Container>
+  </header>
   </div>
 
-    )
+)
 
 Header.propTypes = {
-      siteTitle: PropTypes.string,
-  }
+  siteTitle: PropTypes.string,
+}
 
 Header.defaultProps = {
-      siteTitle: `Hack The Plug`,
-  }
+  siteTitle: `Hack The Plug`,
+}
 
-  export default Header
+export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -7,13 +7,18 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import {
+  useStaticQuery,
+  graphql
+} from "gatsby"
 
 import Header from "./header"
-import StyledBackgroundSection from "./test_component"
+import StyledBackgroundSection from "./bg"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
+const Layout = ({
+  children
+}) => {
+  const data = useStaticQuery(graphql `
     query SiteTitleQuery {
       site {
         siteMetadata {
@@ -23,24 +28,27 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+  return ( <>
+    <Header siteTitle = {
+      data.site.siteMetadata.title
+    }
+    />
 
-      <StyledBackgroundSection className="hello" />
+    <StyledBackgroundSection className = "hello" />
 
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
+    <div style = {
+      {
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `0px 1.0875rem 1.45rem`,
+        paddingTop: 0,
+      }
+    }>
+    <main> {
+      children
+    } </main>
 
-      </div>
-    </>
+    </div> </>
   )
 }
 
