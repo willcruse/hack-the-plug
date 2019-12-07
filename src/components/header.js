06590c87bb943197d520a1398dfbc821372c077c
Will Cruse <willcruse123@gmail.com>
2019-12-07 08:23:57 -0500

    Half way

--- src/components/header.js
+++ src/components/header.js
@@ -9,17 +9,20 @@ import {
   Row,
   Col
 } from "react-bootstrap"
-const Header = () => ( <div>
-  <header style = {
-    {
-      background: `rgba(0, 0, 0, 0.10)`,
-      maxHeight: 50
-      // marginBottom: `1.45rem`
-    }
-  }>
-  <Container fluid>
-    <Row style = {{maxWidth: '50%', marginLeft: '5%'}}>
-      {/*<Col style = {
+const Header = (main) => {
+  if (main===true) {
+    return (
+  <div>
+    <header style={
+      {
+        background: `rgba(0, 0, 0, 0.10)`,
+        maxHeight: 50
+        // marginBottom: `1.45rem`
+      }
+    }>
+      <Container fluid>
+        <Row style={{ maxWidth: '50%', marginLeft: '5%' }}>
+          {/*<Col style = {
         {
           maxHeight: `300px`,
           maxWidth: `300px`
@@ -32,76 +35,77 @@ const Header = () => ( <div>
         <Col> < /Col>*/}
 
 
-        {/* The Link to the About section */ }
-        <Col style = {{alignSelf: `flex-end`, textAlign: 'center'}}>
-        <h2>
-        <Link
-          to = "/"
-          style = {{
-            color: `black`,
-            fontSize: 14
-          }}>About</Link>
-        </h2>
-        </Col>
+          {/* The Link to the About section */}
+          <Col style={{ alignSelf: `flex-end`, textAlign: 'center' }}>
+            <h2>
+              <Link
+                to="/"
+                style={{
+                  color: `black`,
+                  fontSize: 14
+                }}>About</Link>
+            </h2>
+          </Col>
 
 
-        {/* The Link to the committee section */ }
-        <Col style={{alignSelf: `flex-end`, textAlign: 'center'}}>
-        <h2>
-        <Link
-          to="/committee"
-          style={{
-            color: `black`,
-            float: `bottom`,
-            fontSize: 14
-          }}>The team</Link>
-        </h2>
-        </Col>
+          {/* The Link to the committee section */}
+          <Col style={{ alignSelf: `flex-end`, textAlign: 'center' }}>
+            <h2>
+              <Link
+                to="/committee"
+                style={{
+                  color: `black`,
+                  float: `bottom`,
+                  fontSize: 14
+                }}>The team</Link>
+            </h2>
+          </Col>
 
 
-        {/* The Link to the application section */ }
-        <Col style={{alignSelf: `flex-end`, textAlign: 'center'}}>
-        <h2>
-        <Link
-          to="/apply"
-          style={{
-            color: `black`,
-            float: `bottom`,
-            fontSize: 14
-          }}>FAQs</Link>
-        </h2>
-        </Col>
+          {/* The Link to the application section */}
+          <Col style={{ alignSelf: `flex-end`, textAlign: 'center' }}>
+            <h2>
+              <Link
+                to="/apply"
+                style={{
+                  color: `black`,
+                  float: `bottom`,
+                  fontSize: 14
+                }}>FAQs</Link>
+            </h2>
+          </Col>
 
 
-        {/* The Link to the sponsors section */ }
-        <Col style={{alignSelf: `flex-end`, textAlign: 'center'}}>
-        <h2>
-        <Link
-          to="/apply"
-          style={{
-            color: `black`,
-            float: `bottom`,
-            fontSize: 14
-          }}>Sponsors</Link>
-        </h2>
-        </Col>
+          {/* The Link to the sponsors section */}
+          <Col style={{ alignSelf: `flex-end`, textAlign: 'center' }}>
+            <h2>
+              <Link
+                to="/apply"
+                style={{
+                  color: `black`,
+                  float: `bottom`,
+                  fontSize: 14
+                }}>Sponsors</Link>
+            </h2>
+          </Col>
 
 
-    </Row>
-  </Container>
-  </header>
+        </Row>
+      </Container>
+    </header>
   </div>
 
     )
   }
-  return (
-    <div>
-    <header
-      style={{
-        background: `#008dd5`,
+return (
+  <div>
+    <header style={
+      {
+        background: `rgba(0, 0, 0, 0.10)`,
+        maxHeight: 50
         // marginBottom: `1.45rem`
-      }}
-    >
+      }
+    }>
       <Container fluid>
         <Row>
           <Col style={{
@@ -121,25 +125,25 @@ const Header = () => ( <div>
               >
                 Home
       </Link>
-      </h2>
-      </Col>
-      </Row>
+            </h2>
+          </Col>
+        </Row>
       </Container>
     </header>
   </div>
 
-    )
+)
 
       }
 
 Header.propTypes = {
-      siteTitle: PropTypes.string,
-      main: PropTypes.bool,
-  }
+  siteTitle: PropTypes.string,
+  main: PropTypes.bool,
+}
 
 Header.defaultProps = {
-      siteTitle: `Hack The Plug`,
-      main: true,
-  }
+  siteTitle: `Hack The Plug`,
+  main: true,
+}
 
 export default Header
