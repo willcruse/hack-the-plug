6a53ef616fb73f4f79f790c63098f83217d7b1ba
Will Cruse <willcruse123@gmail.com>
2019-12-08 05:35:08 -0500

    Minor cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import StyledBackgroundSection from "../components/bg"
 import '../components/about'
 import Team from "../components/Team"
 import FAQs from "../components/FAQs"
