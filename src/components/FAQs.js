46955f0ba4685ec6887583b9917a9509b6f71339
Will Cruse <willcruse123@gmail.com>
2019-12-26 22:32:26 +0000

    Update application opening

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -12,7 +12,7 @@ class FAQs extends React.Component {
             <div id="FAQs">
                 <h1>FAQs</h1>
                 <QA question="When is Hack The Plug?" ans="Hack The Plug will be held from the 28th to the 29th of March 2020" />
-                <QA question="When will applications open/close?" ans="Applications will open on the 1st of January and close on the 1st of March" />
+                <QA question="When will applications open/close?" ans="Applications will open soon and close on the 1st of March" />
                 <QA question="Will you provide travel reimbusment?" ans="We aim to provide travel reimbursement" />
                 <QA question="How much does Hack the Plug cost to attend?" ans="Hack the Plug is completely free to attend!" />
             </div>
