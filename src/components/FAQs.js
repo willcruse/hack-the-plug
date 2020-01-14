bbd0355cda83c481d95047168569776d67053e82
Oliver <oliver.redeyoff@gmail.com>
2020-01-14 19:10:41 +0000

    Updated about and FAQ section

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -20,9 +20,13 @@ class FAQs extends React.Component {
                   marginBottom: 30
                 }}>FAQs</h1>
                 <QA question="When is Hack The Plug?" ans="Hack The Plug will be held from the 28th to the 29th of March 2020" />
-                <QA question="When will applications open/close?" ans="Applications will open on the 1st of January and close on the 1st of March" />
-                <QA question="Will you provide travel reimbusment?" ans="We aim to provide travel reimbursement" />
-                <QA question="How much does Hack the Plug cost to attend?" ans="Hack the Plug is completely free to attend! We will provide food and drink during the event so no need to worry about that." />
+                <QA question="When will applications open/close?" ans="Applications will open on the 1st of February and close on the 1st of March" />
+                <QA question="Will you provide travel reimbusment?" ans="We aim to provide travel reimbursement, but cannot be sure yet" />
+                <QA question="How much does Hack the Plug cost to attend?" ans="Hack the Plug is completely free to attend! We will provide food and drink during the event so no need to worry about that" />
+                <QA question="How much experience do I need to attend?" ans="Students with any experience level are welcome to apply! We want to make the event enjoyable for beginners and seasoned pros alike" />
+                <p style={{
+                  marginTop: 20
+                }}>Any other questions about hacktheplug? Please feel free to email us at <a style={{fontWeight: 'bold'}}>hacktheplug@gmail.com</a></p>
             </div>
         );
     }
--- src/components/about.js
+++ src/components/about.js
@@ -16,8 +16,11 @@ class About extends React.Component {
           padding: 50
         }}>
           <h1>About Hack the Plug</h1>
-          <p>
-          Hack The Plug is a student run, 24 Hour hackathon held at the University of Bath. Come join us to learn something new, build something cool or start a company!
+          <p>Hack The Plug is a student run, 24 Hour hackathon held at the University of Bath.
+          If you haven't heard of this type of event before, it is a 24 hour long event where teams
+          compete to create the best project. This requires a whole range of skills including
+          team managing, programming and designing.</p>
+          <p>So come join us to learn something new, build something cool or even maybe start a company!
           </p>
         </div>)
     }
