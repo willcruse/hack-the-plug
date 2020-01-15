7b333bf37fb654dd3244161d4dd5021989bed6da
Oliver <oliver.redeyoff@gmail.com>
2020-01-15 15:07:37 +0000

    Fixed space issue

--- src/components/about.js
+++ src/components/about.js
@@ -16,13 +16,8 @@ class About extends React.Component {
           padding: 50
         }}>
           <h1>About Hack the Plug</h1>
-<<<<<<< HEAD
           <p>Hack The Plug is a student run, 24 Hour hackathon held at the <a href="https://www.google.com/maps/place/University+of+Bath/@51.3782261,-2.3285874,17z/data=!3m1!4b1!4m5!3m4!1s0x487181bb49a7bfd7:0x842e7cc0699da7e7!8m2!3d51.3782228!4d-2.3263987" style={{fontWeight: 'bold'}}>University of Bath.</a> If
           you haven't heard of this type of event before, it is a 24 hour long event where teams
-=======
-          <p>Hack The Plug is a student run, 24 Hour hackathon held at the <a href="https://www.google.com/maps/place/University+of+Bath/@51.3782261,-2.3285874,17z/data=!3m1!4b1!4m5!3m4!1s0x487181bb49a7bfd7:0x842e7cc0699da7e7!8m2!3d51.3782228!4d-2.3263987" style={{fontWeight: 'bold'}}>University of Bath. </a>
-          If you haven't heard of this type of event before, it is a 24 hour long event where teams
->>>>>>> 74a5de9d9f714e243367877acb1491b9863bc23c
           compete to create the best project. This requires a whole range of skills including
           team managing, programming and designing.</p>
           <p>So come join us to learn something new, build something cool or even maybe start a company!
