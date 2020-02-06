9c799f27593e8ca69932a03e337e0bd3f4c4b1d7
Will Cruse <willcruse123@gmail.com>
2020-02-06 08:25:29 -0500

    Updated FAQs

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -20,8 +20,8 @@ class FAQs extends React.Component {
                   marginBottom: 30
                 }}>FAQs</h1>
                 <QA question="When is Hack The Plug?" ans="Hack The Plug will be held from the 28th to the 29th of March 2020." />
-                <QA question="When will applications open/close?" ans="Applications will open on the 1st of February and close on the 1st of March." />
-                <QA question="Will you provide travel reimbusment?" ans="We aim to provide travel reimbursement for all participants, but cannot insure this yet. We will update this section as soon as we have more information." />
+                <QA question="When will applications open/close?" ans="Registration will open soon!" />
+                <QA question="Will you provide travel reimbusment?" ans="Unfortunetly we will no be able to provide travel reimbursement this year" />
                 <QA question="How much does Hack the Plug cost to attend?" ans="Hack the Plug is completely free to attend! Additionaly we will provide food and drink during the event for free!" />
                 <QA question="How much experience do I need to attend?" ans="Students with any experience level are welcome to apply! We want to make the event enjoyable for beginners and seasoned pros alike." />
                 <QA question="How many people can be in our team?" ans="Accepted team sizes are 1 to 4 people. If you don't have a team no need to worry, we will find you a group on the day of the event!" />
