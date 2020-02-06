be3d20ee01982e2fe6f392e68d036db396670078
Will Cruse <willcruse123@gmail.com>
2020-02-06 08:23:44 -0500

    Updated FAQs

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -12,8 +12,8 @@ class FAQs extends React.Component {
             <div id="FAQs">
                 <h1>FAQs</h1>
                 <QA question="When is Hack The Plug?" ans="Hack The Plug will be held from the 28th to the 29th of March 2020" />
-                <QA question="When will applications open/close?" ans="Applications will open on the 1st of January and close on the 1st of March" />
-                <QA question="Will you provide travel reimbusment?" ans="We aim to provide travel reimbursement" />
+                <QA question="When will applications open/close?" ans="Applications will open very soon! and close on the 1st of March" />
+                <QA question="Will you provide travel reimbusment?" ans="Unfortunetly we will not be able to provide travel reimbursement" />
                 <QA question="How much does Hack the Plug cost to attend?" ans="Hack the Plug is completely free to attend!" />
             </div>
         );
