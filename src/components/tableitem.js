b63d16aeba76ef687bcab9cb974e9d5babc07d58
Will Cruse <willcruse123@protonmail.com>
2020-02-10 13:56:05 +0000

    Add Timetable

---
+++ src/components/tableitem.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const tableitem = (time, event, location) => (
+    <tr>
+        <td>hello</td>
+        <td>event</td>
+        <td>location</td>
+    </tr>
+)
+
+export default tableitem
---
+++ src/components/timetable.js
@@ -0,0 +1,97 @@
+import React from "react"
+import { Table } from "react-bootstrap"
+import tableitem from "./tableitem"
+const Timetable = () => (
+  <Table>
+    <thead>
+      <tr>
+        <th>Time</th>
+        <th>Event</th>
+        <th>Location</th>
+      </tr>
+    </thead>
+    <tbody>
+    {
+      tableItems.map((item) => {
+        console.log(item)
+        return (
+          <tr>
+            <td>{item.time}</td>
+            <td>{item.event}</td>
+            <td>{item.location}</td>
+          </tr>
+        )
+      })
+    }
+    </tbody>
+  </Table>
+)
+export default Timetable
+
+const tableItems = [
+  {
+    time: "10:00",
+    event: "Registration Opens",
+    location: "Level 1 Foyer"
+  },
+  {
+    time: "10:45",
+    event: "Opening Ceremony",
+    location: "Lecture Theater 1.10"
+  },
+  {
+    time: "12:00",
+    event: "Lunch",
+    location: "Level 1 Foyer"
+  },
+  {
+    time: "12:00",
+    event: "Hacking Starts!!",
+    location: ""
+  },
+  {
+    time: "19:00",
+    event: "Dinner",
+    location: "Level 1 Foyer"
+  },
+  {
+    time: "00:00",
+    event: "Midnight Snack",
+    location: "Level 1 Foyer"
+  },
+  {
+    time: "7:00",
+    event: "Breakfast",
+    location: "Level 1 Foyer"
+  },
+  {
+    time: "12:00",
+    event: "Lunch",
+    location: "Level 1 Foyer"
+  },
+  {
+    time: "12:00",
+    event: "Hacking Ends",
+    location: ""
+  },
+  {
+    time: "13:00",
+    event: "Judging Starts",
+    location: ""
+  },
+  {
+    time: "14:40",
+    event: "Final Judging",
+    location: "Lecture Theater 1.10"
+  },
+  {
+    time: "15:30",
+    event: "Results + Closing Ceremony",
+    location: "Lecture Theater 1.10"
+  },
+  {
+    time: "17:00",
+    event: "Participants Take Off",
+    location: ""
+  }
+]
